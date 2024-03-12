8928903eefeed92fbd443645eb6e8f9ccd527268
Lob77 <uueel1224@gmail.com>
2024-03-12 22:09:20 +0900

    [Feat]스프링으로 전환하기- 싱글톤 패턴

--- core/src/test/java/hello/core/singleton/SingletonService.java
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -1,2 +1,18 @@
-package hello.core.singleton;public class SingletonService {
+package hello.core.singleton;
+
+public class SingletonService {
+
+    private static final SingletonService instance = new SingletonService();
+
+    public static SingletonService getInstance(){
+        return instance;
+    }
+
+    // 생성자를 private으로 막아둔다.
+    private SingletonService(){
+    }
+
+    public void logic(){
+        System.out.println("싱글톤 객체 로직 호출");
+    }
 }
